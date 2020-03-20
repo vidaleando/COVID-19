1b9feb6a77414495b7ba20aff9df624c6cd8ee51
vidaleando <diegovidal2015@gmail.com>
2020-03-20 22:33:10 +0000

    tabla en index

--- assets/javascript/grafica.js
+++ assets/javascript/grafica.js
@@ -4,7 +4,7 @@ var margin = {top: 10, right: 100, bottom: 30, left: 30},
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+var svg = d3.select("#grafica")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
