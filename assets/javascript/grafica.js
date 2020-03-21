6ffb99c87c69f74877e30cd695d1fc6df51450f6
vidaleando <diegovidal2015@gmail.com>
2020-03-21 14:34:29 +0000

    priamide poblacional

--- assets/javascript/grafica.js
+++ assets/javascript/grafica.js
@@ -1,7 +1,7 @@
-// set the dimensions and margins of the graph
+// set the dimensions and margins of the graph, original values w=460, h=400
 var margin = {top: 10, right: 100, bottom: 30, left: 30},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 400 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 
