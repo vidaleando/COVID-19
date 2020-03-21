696b00a5076c05b10c0f9b69ead2a039496fffc0
vidaleando <diegovidal2015@gmail.com>
2020-03-21 01:24:22 +0000

    mapa

--- assets/javascript/mapa.js
+++ assets/javascript/mapa.js
@@ -18,9 +18,8 @@ var colorScale = d3.scaleThreshold()
 
 // Load external data and boot
 d3.queue()
-  
+  .defer(d3.json, "../assets/javascript/mexico.json")
   .await(ready);
-.defer(d3.json, "../assets/javascript/mexico.json")
   //.defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
 //.defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
 
