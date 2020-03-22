654a49bceb4b23fcc7c74ed3184017f14acaf335
vidaleando <diegovidal2015@gmail.com>
2020-03-22 17:34:30 +0000

    Grafica totales

--- assets/javascript/grafica.js
+++ assets/javascript/grafica.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 100, bottom: 30, left: 30},
     width = 460 - margin.left - margin.right,
-    height = 420 - margin.top - margin.bottom;
+    height = 480 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 
