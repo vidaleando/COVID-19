ee7aa14d70c513a7422a804c2d3064be5f7aa9df
vidaleando <diegovidal2015@gmail.com>
2020-03-22 15:19:25 +0000

    Grafica totales

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -23,7 +23,7 @@ d3.csv(url, function(data) {
     var tope=data.length-1;
     console.log(allGroup);
     console.log(tope);
-    console.log(data[4]["Mexico"]);
+    console.log(data[4]);
     // add the options to the button
     d3.select("#selectButton")
       .selectAll('myOptions')
