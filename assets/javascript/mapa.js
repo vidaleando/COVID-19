bd829abdcdd6ec4c8d2293f2765e4ad376736984
vidaleando <diegovidal2015@gmail.com>
2020-03-22 18:23:38 +0000

    Tabla

--- assets/javascript/mapa.js
+++ assets/javascript/mapa.js
@@ -3,8 +3,8 @@
           var urlb="../assets/javascript/mexico.json";
           //var w = 700;
           //var h = 400;
-          var map = d3.select("#mapa");
-          var w = map.node().getBoundingClientRect().width;
+          var mapa = d3.select("#mapa");
+          var w = mapa.node().getBoundingClientRect().width;
           var h = w / 2;
 
 //Define map projection 
