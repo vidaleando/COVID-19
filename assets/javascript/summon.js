bf5aa69364c2f2c2687732da11d257a34dc0cf7e
vidaleando <diegovidal2015@gmail.com>
2020-03-22 12:36:29 +0000

    priamide poblacional

--- assets/javascript/summon.js
+++ assets/javascript/summon.js
@@ -4,5 +4,5 @@
   d3.csv("https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv",function(data) {
    var largo = data.length;
   console.log(data);
-console.log(data[length-1]);
+console.log(data[largo-1]);
 });
