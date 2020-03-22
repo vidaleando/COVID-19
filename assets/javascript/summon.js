0399da494a1a05c688ca7ad7b4b15851fb765dfa
vidaleando <diegovidal2015@gmail.com>
2020-03-22 14:49:51 +0000

    priamide poblacional

--- assets/javascript/summon.js
+++ assets/javascript/summon.js
@@ -1,42 +1,20 @@
   
 // SUMMON
 // URL
-urlTotal="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv";
+ var urlTotal="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv",
 urlRecu="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_recuperados.csv";
 
 //totales
-  d3.csv("https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv",function(data) {
+  d3.csv(urlTotal,function(data) {
    var largo = data.length;
    var tope =largo-1;
 var div = document.getElementById('totales');
     div.innerHTML = data[tope]["Mexico"];
 });
-/*
-  d3.queue()
-.defer(d3.csv, urlTotal,function(data) {
+
+  d3.csv(urlRecu,function(data) {
    var largo = data.length;
    var tope =largo-1;
-var div = document.getElementById('totales');
-    div.innerHTML = data[tope]["Mexico"];
-})
-.defer(d3.csv, urlRecu,function(data) {
-   var largo = data.length;
-   var tope =largo-1;
-   console.log(data[tope]["Mexico"]);
 var div = document.getElementById('recu');
     div.innerHTML = data[tope]["Mexico"];
-})
-.await(semaforo);
-semaforo(error, covid19_mex_casos_totales, covid19_mex_casos_totales) {
-    if (error) {
-        console.error('Valió madres por esto: ' + error);
-    }
-    else {
-       var largo = data.length;
-   var tope =largo-1;
-   console.log(data[tope]["Mexico"]);
-var div = document.getElementById('recu');
-    div.innerHTML = data[tope]["Mexico"];
-           }
-}
-*/
+});
