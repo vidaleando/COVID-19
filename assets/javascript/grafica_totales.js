20a1c0847143cf07fbd2a82704eadf591a93051e
vidaleando <diegovidal2015@gmail.com>
2020-03-22 23:59:18 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -45,7 +45,7 @@ set_responsive();
 
 var svg = d3.select("#grafica")
   .append("svg")
-      .attr("viewBox","0 0 1000 1060")  
+      .attr("viewBox","0 0 400 460")  
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
