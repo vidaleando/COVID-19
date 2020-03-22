c6d32306e8ea45711726083db5ec0f9d35f8a8ac
vidaleando <diegovidal2015@gmail.com>
2020-03-22 17:30:07 +0000

    Grafica totales

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -71,7 +71,7 @@ var yyyy = today.getFullYear();
 
     // Add Y axis      //
     var y = d3.scaleLinear()
-    .domain( [0,300])
+    .domain( [0,d3.max(data, function(d){return +d.Mexico;  })])
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y));
