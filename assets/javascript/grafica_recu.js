dfa288a4e8487c62c37c5649e2b95130ce0cda1b
vidaleando <diegovidal2015@gmail.com>
2020-03-23 18:55:39 +0000

    Tabla

--- assets/javascript/grafica_recu.js
+++ assets/javascript/grafica_recu.js
@@ -2,7 +2,7 @@
 var margin = {top: 10, right: 30, bottom: 30, left: 30},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom; 
-var url="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_recuperados.csv";
+var url="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_recuperados.csv";
 
 
 var svg = d3.select("#grafica_recu")
