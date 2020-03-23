71f376393c5ae0830b4751480ff3a7145dd15f1b
vidaleando <diegovidal2015@gmail.com>
2020-03-23 19:18:14 +0000

    Tabla

--- assets/javascript/summon.js
+++ assets/javascript/summon.js
@@ -23,6 +23,6 @@ var div = document.getElementById('recu');
  	var tot = document.getElementById('totales').innerHTML,
  		rec = document.getElementById('recu').innerHTML;
  		console.log(tot); 
- 	conole.log(document.getElementById('activos').innerHTML=+tot-rec);
+ 	console.log(document.getElementById('activos').innerHTML=+tot-rec);
  }
 
