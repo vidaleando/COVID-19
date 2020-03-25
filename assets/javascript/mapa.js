f01f27e9a847358b16da2d374d1f72a28deda60a
vidaleando <diegovidal2015@gmail.com>
2020-03-25 19:32:42 +0000

    Mapa tooltip

--- assets/javascript/mapa.js
+++ assets/javascript/mapa.js
@@ -53,7 +53,7 @@ var mapSvg = d3.select("#mapa")
 //load GeoJson data
 d3.json("https://raw.githubusercontent.com/vidaleando/COVID-19/master/assets/javascript/prueba.geojson", function(json) {
     feat = json.features;
-    console.log(feat[0]["properties"]["name"])
+    console.log(feat[0]["properties"]["name"]);
     // bind data
     mapSvg.selectAll("path")
         .data(json.features)
@@ -65,7 +65,7 @@ d3.json("https://raw.githubusercontent.com/vidaleando/COVID-19/master/assets/jav
             navMap.transition()    
                 .duration(200)    
                 .style("opacity", .9);    
-            navMap.html("<em>" +d.features.name+ "</em>"+ "<br/> <p class='text-primary'>"  + d.Mexico_pais + "</p>")  
+            navMap.html("<em>" + d.features.name + "</em>"+ "<br/> <p class='text-primary'>"  + d.Mexico_pais + "</p>")  
                 .style("left", (d3.event.pageX) + "px")   
                 .style("top", (d3.event.pageY - 28) + "px");  
             })          
