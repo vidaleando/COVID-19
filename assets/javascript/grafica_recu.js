de7f1c53b49b482cdb5d39fbd041cf169c94bdcc
vidaleando <diegovidal2015@gmail.com>
2020-03-25 15:28:06 +0000

    Tabla

--- assets/javascript/grafica_recu.js
+++ assets/javascript/grafica_recu.js
@@ -4,29 +4,29 @@ var margin = {top: 10, right: 10, bottom: 0, left: 50},
     height = 450 - margin.top - margin.bottom; 
 var url="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_recuperados.csv";
 
-var tip = d3.select("#grafica_totales").append("div") 
+var tip = d3.select("#grafica_recu").append("div") 
     .attr("class", "tip")       
     .style("opacity", 0);
 
 var svgR = d3.select("#grafica_recu")
   .append("svg")
-  //.attr("viewBox","0 0 460 400")
-  .attr("width","460")
-  .attr("height","430")
+  .attr("width", width+margin.left+margin.right+80)
+  .attr("height",height+margin.top+margin.bottom+120)
   .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+          "translate(" +(20+ margin.left )+ "," + margin.top + ")");
 
 //Read the data
 d3.csv(url, function(data) {
 
     // List of groups (here I have one group per column)
     var allGroup = ["Lineal","Logarítmica"];
-    //var allGroupb = {Lineal:Mexico,"Logar.attr("viewBox","0 0 460 400")ítmica":Mexico_log10};
+    //var allGroupb = {Lineal:Mexico,"Logarítmica":Mexico_log10};
     var tope=data.length-1;
+
     data.forEach(function(d) {
                d.Fecha = new Date(d.Fecha);
-               d.Mexico = +d.Mexico;
+               d.Mexico_pais = +d.Mexico_pais;
             });
     //console.log(allGroupb);
     // add the options to the button
@@ -42,20 +42,26 @@ d3.csv(url, function(data) {
  // define the x scale (horizontal)
 
       var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
+/*var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
-//today = mm + '/' + dd + '/' + yyyy;
+//today = mm + '/' + dd + '/' + yyyy; */
+
  formatMonth = d3.timeFormat("%b"), //%m
     formatDay = d3.timeFormat("%d");
 
-        var mindate = new Date(2020,1,28);
 
+ formatMes = d3.timeFormat("%b"),
+    formatDia = d3.timeFormat("%d");
+
+  var mindate = new Date(2020,1,28);
+console.log(formatMes(mindate));
+console.log(mindate);
     // Add X axis --> it is a date format
     var x = d3.scaleTime()
       .domain([mindate,today])
-      .range([ 0, width ]);
+      .range([ 0, width-10 ]);
 
     svgR.append("g")
       .attr("transform", "translate(0," + height + ")")
@@ -67,7 +73,7 @@ var yyyy = today.getFullYear();
         .attr("dy", ".15em")
         .attr("transform", "rotate(-65)");
 
-        // text label for the x axis
+  // text label for the x axis
   svgR.append("text")             
       .attr("transform",
             "translate(" + (width/2) + " ," + 
@@ -75,21 +81,22 @@ var yyyy = today.getFullYear();
       .style("text-anchor", "middle")
       .text("Fecha");
 
-    // Add Y axis      //
+    // Add Y axis      
     var y = d3.scaleLinear()
-    .domain( [0,d3.max(data, function(d){return d.Mexico;  })])
-      .range([ height, 0 ]);
+    .domain( [0,d3.max(data, function(d){return d.Mexico_pais;  })])
+      .range([ height-10, 0 ]);
     svgR.append("g")
       .call(d3.axisLeft(y));
 
-       // text label for the y axis
+    // text label for the y axis
   svgR.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", -20 - margin.left)
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
-      .text("Casos");      
+      .text("Casos");   
+  
 
     // Initialize line with group a
     var line = svgR
@@ -98,22 +105,22 @@ var yyyy = today.getFullYear();
         .datum(data)
         .attr("d", d3.line()
           .x(function(d) { return x(d.Fecha) })
-          .y(function(d) { return y(+d.Mexico) })
+          .y(function(d) { return y(+d.Mexico_pais) })
         )
         .attr("stroke", "#1f9bcf")
         .style("stroke-width", 3)
         .style("fill", "none")
 
     // Initialize dots with group a
-    var dot = svgR
-      .selectAll('circle')
+    var dot = svgR.selectAll('circle')
       .data(data)
       .enter()
       .append('circle')
-        .attr("cx", function(d) { return x(+d.Fecha) })
-        .attr("cy", function(d) { return y(+d.Mexico) })
+        .attr("cx", function(d) { return x(d.Fecha) })
+        .attr("cy", function(d) { return y(+d.Mexico_pais) })
         .attr("r", 4)
-        .style("fill", "#1F9BCF").on("mouseover", function(d) {    
+        .style("fill", "#1F9BCF")
+        .on("mouseover", function(d) {    
             tip.transition()    
                 .duration(200)    
                 .style("opacity", .9);    
@@ -160,6 +167,4 @@ var yyyy = today.getFullYear();
     })
 
 });
-
-
 
