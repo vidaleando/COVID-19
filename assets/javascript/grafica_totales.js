9daf33995adc1ecdaf6616aafb638eac5ac10539
vidaleando <diegovidal2015@gmail.com>
2020-03-25 15:17:56 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -14,7 +14,7 @@ var svgT = d3.select("#grafica_totales")
   .attr("height",height+margin.top+margin.bottom+120)
   .append("g")
     .attr("transform",
-          "translate(" +(50+ margin.left )+ "," + margin.top + ")");
+          "translate(" +(30+ margin.left )+ "," + margin.top + ")");
 
 //Read the data
 d3.csv(url, function(data) {
