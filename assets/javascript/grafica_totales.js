df76b41037ed8f86601a3a1fd485b5766bde481e
vidaleando <diegovidal2015@gmail.com>
2020-03-25 14:06:47 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 10, bottom: 10, left: 50},
+var margin = {top: 10, right: 10, bottom: 0, left: 50},
     width = 500 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom; 
 var url="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv";
