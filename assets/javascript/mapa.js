b247288500e035ffa77c096000302ec3dd8a44eb
vidaleando <diegovidal2015@gmail.com>
2020-03-25 19:35:34 +0000

    Mapa tooltip

--- assets/javascript/mapa.js
+++ assets/javascript/mapa.js
@@ -61,12 +61,11 @@ d3.json("https://raw.githubusercontent.com/vidaleando/COVID-19/master/assets/jav
         .append("path")
         .attr("d", path)
         //.on("mouseover", hover);
-        .on("mouseover", function(d) {
-        console.log(d.properties);    
+        .on("mouseover", function(d) {  
             navMap.transition()    
                 .duration(200)    
                 .style("opacity", .9);    
-            navMap.html("<em>" + d.properties.name + "</em>"+ "<br/> <p class='text-primary'>"  + d.Mexico_pais + "</p>")  
+            navMap.html("<em>" + d.properties.name + "</em>"+ "<br/> <p class='text-primary'>"  + d.properties.totales + "</p>")  
                 .style("left", (d3.event.pageX) + "px")   
                 .style("top", (d3.event.pageY - 28) + "px");  
             })          
