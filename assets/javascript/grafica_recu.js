1bc5f941142359d56c805bf76eaa6b2db7acacd3
vidaleando <diegovidal2015@gmail.com>
2020-03-25 17:03:38 +0000

    Graficas con tooltip

--- assets/javascript/grafica_recu.js
+++ assets/javascript/grafica_recu.js
@@ -91,7 +91,7 @@ console.log(mindate);
     // text label for the y axis
   svgR.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", -20 - margin.left)
+      .attr("y", -0 - margin.left)
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
