02c4f13ad7dbff2603e207869e2fe593f2ab0363
vidaleando <diegovidal2015@gmail.com>
2020-03-25 19:23:51 +0000

    Mapa tooltip

--- assets/javascript/mapa.js
+++ assets/javascript/mapa.js
@@ -53,7 +53,7 @@ var mapSvg = d3.select("#mapa")
 //load GeoJson data
 d3.json("https://raw.githubusercontent.com/vidaleando/COVID-19/master/assets/javascript/prueba.geojson", function(json) {
     feat = json.features;
-    console.log(feat[0]);
+    console.log(feat[0][0]);
     // bind data
     mapSvg.selectAll("path")
         .data(json.features)
