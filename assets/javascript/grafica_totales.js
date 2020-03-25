517cc0577869208ec4e13b6a1d47456c74e50d40
vidaleando <diegovidal2015@gmail.com>
2020-03-25 14:23:25 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 10, bottom: 0, left: 50},
     width = 500 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom; 
+    height = 420 - margin.top - margin.bottom; 
 var url="https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv";
 
 var tip = d3.select("#grafica_totales").append("div") 
