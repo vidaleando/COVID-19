8d5346d87609bf77203f8f7c7123b1a734612fef
vidaleando <diegovidal2015@gmail.com>
2020-03-25 15:47:44 +0000

    Graficas con tooltip

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -91,7 +91,7 @@ console.log(mindate);
     // text label for the y axis
   svgT.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", -10 - margin.left)
+      .attr("y", -0 - margin.left)
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
