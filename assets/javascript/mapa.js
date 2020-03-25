498813569277ff764f716b432ef3af8eafff7ac7
vidaleando <diegovidal2015@gmail.com>
2020-03-25 19:42:58 +0000

    Mapa tooltip

--- assets/javascript/mapa.js
+++ assets/javascript/mapa.js
@@ -65,8 +65,8 @@ d3.json("https://raw.githubusercontent.com/vidaleando/COVID-19/master/assets/jav
             navMap.transition()    
                 .duration(200)    
                 .style("opacity", .9);    
-            navMap.html("<h6>" + d.properties.name + "</h6>"+ "<br/> <p class='text-danger'> Totales: "  + d.properties.totales + "</p>"
-              +"<br/> <p class='text-warning'> Activos: "  + d.properties.activos + "</p>"+"<br/> <p class='text-primary'> Muertes: "  + d.properties.muertes + "</p>")  
+            navMap.html("<h6>" + d.properties.name + "</h6>"+ "<p class='text-danger'> Totales: "  + d.properties.totales + "</p>"
+              +"<p class='text-warning'> Activos: "  + d.properties.activos + "</p>"+"<p class='text-primary'> Muertes: "  + d.properties.muertes + "</p>")  
                 .style("left", (d3.event.pageX) + "px")   
                 .style("top", (d3.event.pageY - 28) + "px");  
             })          
