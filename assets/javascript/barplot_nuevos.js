61bd4d4209aa4f928222c8f33b4e1013eaa8533a
vidaleando <diegovidal2015@gmail.com>
2020-03-26 13:18:16 +0000

    Tabla

--- assets/javascript/barplot_nuevos.js
+++ assets/javascript/barplot_nuevos.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 10, bottom: 0, left: 53},
+var margin = {top: 10, right: 10, bottom: 0, left: 30,
    // width = 460 - margin.left - margin.right,
     //height = 400 - margin.top - margin.bottom;
     width=$("#barplot_nuevos").width(),
