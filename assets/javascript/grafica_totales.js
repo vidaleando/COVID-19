c6523f365dc9429221068cbdc21f7e76c79f6bab
vidaleando <diegovidal2015@gmail.com>
2020-03-26 13:07:56 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -90,7 +90,7 @@ var yyyy = today.getFullYear();
 
     // text label for the y axis
   svgT.append("text")
-      .attr("transform", "rotate(-90)")
+      //.attr("transform", "rotate(-90)")
       .attr("y", -0 - margin.left)
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
