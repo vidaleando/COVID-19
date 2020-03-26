930af21369dc9a0f2fc72722cb21204750c9a612
vidaleando <diegovidal2015@gmail.com>
2020-03-26 00:20:38 +0000

    Mapa tooltip

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -190,7 +190,7 @@ var yyyy = today.getFullYear();
   /* Create a shared transition for anything we're animating */
   var t = svgT.transition()
     .delay(750)
-    .duration(400)
+    .duration(1000)
     .ease(d3.easeLinear)
     .on('end', function() {
       d3.select('line.guide')
