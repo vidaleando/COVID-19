6a22603b2949aef6803f1592612708e04ffb1516
vidaleando <diegovidal2015@gmail.com>
2020-03-26 21:31:50 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -98,8 +98,8 @@ var fase12=new Date(2020,2,23);
     .attr("class", "line")
     .style("stroke", "red")
     .attr(d3.line)
-    .attr("x1", x(  Date(2020,2,23) ) )
-    .attr("x2", x(  Date(2020,2,23) ) )
+    .attr("x1", x(  fase12 ) )
+    .attr("x2", x(  fase12 ) )
     .attr("y1", y( y.domain()[0]) )   // whatever the y-val should be
     .attr("y2", y( y.domain()[1]) )
     .attr("stroke", "red")
