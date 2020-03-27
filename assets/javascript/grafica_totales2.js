0fb20c770dc989fc58c04926ff8857690cde05fa
vidaleando <diegovidal2015@gmail.com>
2020-03-27 22:10:21 +0000

    Tabla

--- assets/javascript/grafica_totales2.js
+++ assets/javascript/grafica_totales2.js
@@ -19,7 +19,7 @@ var svgT = d3.select("#grafica_totales")
           "translate(" +(0+ margin.left )+ "," + (margin.top) + ")");
 
 //Read the data
-queue()
+d3.queue()
   .defer(d3.csv, 'https://raw.githubusercontent.com/LeonardoCastro/COVID19-Mexico/master/data/series_tiempo/covid19_mex_casos_totales.csv')
   .defer(d3.csv, 'https://github.com/blas-ko/COVID-19_Coupled-Epidemics/blob/master/results/covid19_mex_proyeccion_susana_00.csv')
   .await(grafica);
