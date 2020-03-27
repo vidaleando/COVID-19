8e23303dd151b57abe19e48426e86bccf7986d04
vidaleando <diegovidal2015@gmail.com>
2020-03-27 13:02:01 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -199,7 +199,7 @@ var fase12=new Date(2020,2,23);
       .style("text-anchor", "end")
       .text("Comienza la fase 2")
       .attr("stroke", "#000000")
-      .attr("font-size","10");           
+      .attr("font-weight","100");           
 
 // Animation
   /* Add 'curtain' rectangle to hide entire graph */
