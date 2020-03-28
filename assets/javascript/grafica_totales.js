561409c43d788e6279d18301ae01729e663b8479
vidaleando <diegovidal2015@gmail.com>
2020-03-28 13:10:23 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -298,8 +298,8 @@ d3.csv(url, function(data) {
     // Animation
     /* Add 'curtain' rectangle to hide entire graph */
     var curtain = svgT.append('rect')
-        .attr('x', -1 * (w+30))//width
-        .attr('y', -1 * h)//height
+        .attr('x', -1 * w)//width
+        .attr('y', -1 * (h-10))//height
         .attr('height', h+5)//height
         .attr('width', w)//width
         .attr('class', 'curtain')
