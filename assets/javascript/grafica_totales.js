1af30f5a1e128310e0ae1342b4cb2c5c35d4de0c
vidaleando <diegovidal2015@gmail.com>
2020-03-28 14:38:14 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -295,7 +295,7 @@ d3.csv(url, function(data) {
         .attr("font-family", "sans-serif");
  //Leyenda
 var coordX =(x(x.domain()[1])-(margin.left+margin.right))*0.15,
-coordY =  (y(y.domain()[1])+margin.top);
+coordY =  (y(y.domain()[1])+margin.top)*0.8;
 offset=30;
 
 //Leyenda susi_00
