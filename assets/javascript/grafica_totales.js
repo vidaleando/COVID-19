e7c941a229cca7b7eee457d025c89647c85ea8c2
vidaleando <diegovidal2015@gmail.com>
2020-03-28 13:01:48 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -19,9 +19,9 @@
     }
 
     var margin = {
-            top: 0,
+            top: 10,
             right: 10,
-            bottom: 45,
+            bottom: 40,
             left: 40
         },
    w = (w- (margin.left + margin.right) );
