79a3992955946bcd14dad7943b7e28edc590d46e
vidaleando <diegovidal2015@gmail.com>
2020-03-28 10:29:34 +0000

    Tabla

--- assets/javascript/grafica_totales.js
+++ assets/javascript/grafica_totales.js
@@ -119,7 +119,7 @@ var fase12=new Date(2020,2,23);
         .attr("d", d3.line()
           .x(function(d) { return x(d.Fecha) })
           .y(function(d) { return y(+d.México) })
-          .defined(function(d) {console.log(y(d.México));return y(d.México) !== null; })
+          .defined(function(d) {console.log(y(d.México)); console.log(d.México);return y(d.México) !== null; })
         )
         .attr("stroke", "#1f9bcf")
         .style("stroke-width", 3)
